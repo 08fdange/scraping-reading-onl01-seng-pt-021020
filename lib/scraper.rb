e5ecddb0a444a7e270048d1fc3b070b68e59e9dd
Frank  D'Angelo <08fdange@gmail.com>
2020-03-15 21:35:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://www.google.com/")
 
 doc = Nokogiri::HTML(html)
-change_things = (doc.css(".headline-26OIBN").text.strip)
+google_logo = (doc.css("#hplogo"))
 
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+#courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
-courses.each do |course|
-  puts course.text.strip
-end
+puts google_logo
